2518c2d36f605f3a402b5ae48be21d1ff58f8d0b
Jan Weber <jan.weber123@googlemail.com>
2023-09-09 12:22:00 +0200

    Class Game with init self and function run

--- game.py
+++ game.py
@@ -1,18 +1,23 @@
 import sys
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
 
-pygame.display.set_caption('ninja game')
-screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption('ninja game')
+        self.screen = pygame.display.set_mode((640, 480))
 
-clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    pygame.display.update()
-    clock.tick(60)
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
