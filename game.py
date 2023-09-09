c86ca566a28651997917ad27ed4cd5d1f7f290e9
Jan Weber <jan.weber123@googlemail.com>
2023-09-09 12:16:33 +0200

    message

--- game.py
+++ game.py
@@ -3,10 +3,7 @@ import pygame
 
 pygame.init()
 
-<<<<<<< HEAD
 pygame.display.set_caption('ninja game')
-=======
->>>>>>> d4df46a801ce42b97997beafa445d1b237588673
 screen = pygame.display.set_mode((640, 480))
 
 clock = pygame.time.Clock()
