8409c3c715e21a632503f7faddb74c8bd453d01a
Jan Weber <jan.weber123@googlemail.com>
2023-09-10 02:47:27 +0200

    collide detect

--- game.py
+++ game.py
@@ -12,15 +12,30 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
 
         self.img_pos = [160, 200]
         self.movement = [False, False]
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
+
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.screen.blit(self.img, self.img_pos)
+
+            img_r = pygame.Rect(
+                self.img_pos[0],
+                self.img_pos[1],
+                self.img.get_width(),
+                self.img.get_height(),
+            )
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
